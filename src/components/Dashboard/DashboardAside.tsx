e2b55679f5a7cd3ff299d14b8216e0814060747f
杨伟杰 <sia-fl@outlook.com>
2024-10-18 04:45:05 +0800

    pref: 添加项目 & 用户列表

--- src/components/Dashboard/DashboardAside.tsx
+++ src/components/Dashboard/DashboardAside.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import AsideItem from './AsideItem';
 import AsideSubItem from './AsideSubItem';
 
@@ -8,16 +8,60 @@ interface DashboardAsideProps {
 
 function DashboardAside({ isOpen }: DashboardAsideProps) {
   const [showTooltip, setShowTooltip] = useState(false);
+  const [showProjectList, setShowProjectList] = useState(true);
+  const [selectedProject, setSelectedProject] = useState<string>('1');
+
+  // 项目列表数据
+  const projectList = [
+    { id: '1', name: '我的项目 1', color: 'bg-primary-600' },
+    { id: '2', name: '我的项目 2', color: 'bg-teal-400' },
+    { id: '3', name: '他人的项目 1', color: 'bg-orange-300' },
+    { id: '4', name: '他人的项目 2', color: 'bg-purple-500' },
+  ];
+
+  // 成员列表数据（示例）
+  const memberList = [
+    {
+      name: '张三',
+      email: 'zhangsan@example.com',
+      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png',
+    },
+    {
+      name: '李四',
+      email: 'lisi@example.com',
+      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png',
+    },
+    {
+      name: '王五',
+      email: 'wangwu@example.com',
+      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png',
+    },
+    {
+      name: '赵六',
+      email: 'zhaoliu@example.com',
+      avatar: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/thomas-lean.png',
+    },
+  ];
+
+  const PROJECT_ITEM_HEIGHT = 40; // 固定项目项的高度为40px
+  const PROJECT_ITEM_GAP = 8; // 项目之间的总间距
+  const MAX_VISIBLE_ITEMS = 3.5; // 最大可见项目数（3个半）
 
   return (
     <aside
       className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
         isOpen ? 'translate-x-0' : '-translate-x-full'
-      } bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700 md:pt-14 pb-14 md:pb-0 flex flex-col`}
+      } bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700 md:pt-14 pb-14 md:pb-0 flex flex-col overflow-hidden`}
       aria-label="Sidenav"
       id="drawer-navigation"
     >
-      <div className="overflow-y-auto py-5 px-3 flex-grow bg-white dark:bg-gray-800">
+      <div
+        className="flex-grow overflow-y-auto px-3 bg-white dark:bg-gray-800 overscroll-contain"
+        style={{ overscrollBehavior: 'contain' }}
+      >
+        <div className="flex justify-between pb-0.5 pt-1 items-center pl-2 mt-5 mb-2 text-sm font-medium text-gray-500 uppercase dark:text-gray-400">
+          <h3>项目管理</h3>
+        </div>
         <ul className="space-y-2">
           <AsideItem
             href="#"
@@ -33,10 +77,10 @@ function DashboardAside({ isOpen }: DashboardAsideProps) {
                 <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
               </svg>
             }
-            text="Overview"
+            text="概览"
           />
           <AsideItem
-            text="Pages"
+            text="页面"
             icon={
               <svg
                 aria-hidden="true"
@@ -58,15 +102,144 @@ function DashboardAside({ isOpen }: DashboardAsideProps) {
               <AsideSubItem key="calendar" href="#" text="Calendar" />,
             ]}
           />
-          {/* 其他 AsideItem 组件 */}
-        </ul>
-
-        <ul className="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
-          {/* 其他 AsideItem 组件 */}
         </ul>
       </div>
 
-      <div className="flex justify-center p-4 space-x-4 w-full bg-white dark:bg-gray-800 z-20">
+      <div
+        className="px-3 border-t border-gray-200 bg-white dark:bg-gray-800 overflow-y-auto overscroll-contain"
+        style={{ overscrollBehavior: 'contain' }}
+      >
+        <div className="w-full mt-5">
+          <div
+            className="grid max-w-xs grid-cols-2 gap-1 p-1 mx-auto my-2 bg-gray-100 rounded-lg dark:bg-gray-600"
+            role="group"
+          >
+            <button
+              type="button"
+              className={`px-3 py-1.5 font-medium rounded-lg ${
+                showProjectList
+                  ? 'text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900'
+                  : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700'
+              }`}
+              onClick={() => setShowProjectList(true)}
+            >
+              项目列表
+            </button>
+            <button
+              type="button"
+              className={`px-3 py-1.5 font-medium rounded-lg ${
+                !showProjectList
+                  ? 'text-white bg-gray-900 dark:bg-gray-300 dark:text-gray-900'
+                  : 'text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700'
+              }`}
+              onClick={() => setShowProjectList(false)}
+            >
+              本项目成员
+            </button>
+          </div>
+        </div>
+        <div className="pl-2 my-4 text-sm text-gray-500 dark:text-gray-400 dark:border-gray-700">
+          <h3>{showProjectList ? '所有项目' : '项目成员'}</h3>
+        </div>
+        <div
+          className="relative overflow-hidden border border-gray-200 dark:border-gray-700 rounded-lg"
+          style={{ height: `${MAX_VISIBLE_ITEMS * (PROJECT_ITEM_HEIGHT + PROJECT_ITEM_GAP)}px` }}
+        >
+          <div
+            className={`absolute inset-0 transition-transform duration-300 ease-in-out ${
+              showProjectList ? 'translate-x-0' : '-translate-x-full'
+            }`}
+          >
+            <div className="overflow-y-auto h-full">
+              <ul className="pl-2 pb-2 space-y-2 relative">
+                <div
+                  className="absolute left-0 w-[calc(100%-16px)] ml-[8px] bg-primary-100 dark:bg-primary-900 rounded-lg transition-all duration-300 ease-in-out"
+                  style={{
+                    height: `${PROJECT_ITEM_HEIGHT}px`,
+                    transform: `translateY(${
+                      selectedProject
+                        ? (parseInt(selectedProject) - 1) * (PROJECT_ITEM_HEIGHT + PROJECT_ITEM_GAP)
+                        : 0
+                    }px)`,
+                    opacity: selectedProject ? 1 : 0,
+                  }}
+                ></div>
+                {projectList.map((project) => (
+                  <li key={project.id} style={{ height: `${PROJECT_ITEM_HEIGHT}px` }}>
+                    <a
+                      href="#"
+                      className={`flex w-[calc(100%-8px)] items-center text-base font-medium rounded-lg transition duration-300 px-3 py-2 relative z-10 ${
+                        selectedProject === project.id
+                          ? 'text-primary-700 dark:text-primary-300'
+                          : 'text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700'
+                      }`}
+                      onClick={() => setSelectedProject(project.id)}
+                    >
+                      <span className={`w-4 h-4 ${project.color} rounded`}></span>
+                      <span className="ml-3">{project.name}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div
+            className={`absolute inset-0 transition-transform duration-300 ease-in-out ${
+              showProjectList ? 'translate-x-full' : 'translate-x-0'
+            }`}
+          >
+            <div className="overflow-y-auto h-full">
+              <ul className="pl-2 space-y-2">
+                {memberList.map((member, index) => (
+                  <li key={index} style={{ height: `${PROJECT_ITEM_HEIGHT}px` }}>
+                    <div className="flex items-center text-base font-medium text-gray-900 rounded-lg dark:text-white px-3 py-2">
+                      <span className="w-4 h-4 flex-shrink-0">
+                        <img
+                          className="w-full scale-150 -translate-x-1 h-full rounded-full object-cover"
+                          src={member.avatar}
+                          alt={`${member.name} avatar`}
+                        />
+                      </span>
+                      <div className="ml-3 overflow-hidden flex-grow">
+                        <span className="text-sm font-medium truncate block">{member.name}</span>
+                        <span className="text-xs text-gray-500 dark:text-gray-400 truncate block">
+                          {member.email}
+                        </span>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="my-2">
+          <a
+            href="#"
+            className="flex items-center pl-5 text-base font-medium text-gray-900 rounded-lg transition duration-75 dark:text-gray-200 group px-3 py-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+          >
+            <span className="w-4 h-4 rounded border border-gray-200 dark:border-gray-400 flex items-center justify-center">
+              <svg
+                className="w-3 h-3"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <span className="ml-3 text-gray-500 dark:text-gray-400 hover:underline">
+              {showProjectList ? '创建项目' : '邀请成员'}
+            </span>
+          </a>
+        </div>
+      </div>
+
+      <div className="flex justify-center p-4 py-2 space-x-4 border-t border-gray-200 w-full bg-white dark:bg-gray-800 z-20">
         <a
           href="#"
           className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600"
